b1c51c5890f1d6cfec524e4f088840cdcbd8796b
Caio Ergos <caioergos@gmail.com>
2017-09-03 15:16:48 -0300

    add plug to set user in router

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -16,6 +16,12 @@ defmodule Discuss.AuthController do
     signin(conn, changeset)
   end
 
+  def signout(conn, _params) do
+    conn
+    |> configure_session(drop: true)
+    |> redirect(to: topic_path(conn, :index))
+  end
+
   defp insert_or_update(changeset) do
     case Repo.get_by(User, Map.take(changeset, [:email, :provider])) do
       nil  -> Repo.insert(changeset)
