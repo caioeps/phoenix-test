173e47f9868be2192084fa850bf8e84338c63046
Caio Ergos <caioergos@gmail.com>
2017-09-03 14:56:36 -0300

    implement oauth with github

---
+++ web/models/user.ex
@@ -0,0 +1,17 @@
+defmodule Discuss.User do
+  use Discuss.Web, :model
+
+  schema "users" do
+    field :email, :string
+    field :provider, :string
+    field :github_token, :string
+
+    timestamps()
+  end
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:provider, :github_token])
+    |> validate_required([:provider, :github_token])
+  end
+end
